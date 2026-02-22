b075a14cda32a972235b21581d602b61a9f4119f
Mihai Botezat <117821432+mihu19@users.noreply.github.com>
2026-02-22 12:51:24 +0200

    Delete algorithms.h

--- algorithms.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-
-void bubbleSort(std::vector<int>& arr);
-void insertionSort(std::vector<int>& arr);
-void shellSort(std::vector<int>& arr);
-void mergeSort(std::vector<int>& arr, int left, int right);
